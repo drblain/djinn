78d8edece827fbac475ed942011968c678fe9365
drblain <drblain22@gmail.com>
2025-07-12 09:41:24 -0400

    Skeletons

---
+++ lib/inc/djinn/contexts/GLFWContext.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "djinn/Context.hpp"
+
+#include <GLFW/glfw3.h>
+
+namespace djinn
+{
+
+class GLFWContext : public Context
+{
+
+};
+
+class GLFWContextManager : public ContextManager
+{
+
+};
+
+}
